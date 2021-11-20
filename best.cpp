5faad1d08f0f7c4b60d7a6fc3945f46ee187987d
Emmaa <emma@emma.emma>
2021-11-20 20:40:17 +0100

    adjacency matrix

--- best.cpp
+++ best.cpp
@@ -5,22 +5,36 @@
 #include <math.h>
 #include <random>
 #include <stack>
-#include <chrono>
 #include <algorithm>
  
 using namespace std;
-using namespace std::chrono;
 
-
-int printMatrix(vector<vector<double>> matrix){
+//Just for printing methods (felsökning typ)
+void printMatrix(vector<vector<int>> matrix){
     for (int i = 0; i < matrix.size(); i++){
             for (int j = 0; j < matrix[i].size(); j++){
             cout << matrix[i][j] << ' ';
             }
             cout << endl;
         }
-    return 0;
 }
+
+//Create initial adjacency matrix
+//If self set distance to "infinity"
+vector<vector<int>> getAdjMatrix(vector<vector<double>> coordinates, int noVertices){
+    vector<vector<int>> adjMatrix(noVertices, vector<int>(noVertices));
+    for (int i = 0; i < noVertices; i++) {
+        for (int j = 0; j <= i; j++) {
+            if(i == j) {
+                adjMatrix[i][j] = 100000000;
+            } else {
+                adjMatrix[i][j] = round(sqrt(pow(coordinates[i][0] - coordinates[j][0], 2)+ pow(coordinates[i][1] - coordinates[j][1], 2)));
+                adjMatrix[j][i] = adjMatrix[i][j];
+            }
+        }
+    }
+    return adjMatrix;
+ }
 
 int main(){
     int noVertices;
@@ -31,9 +45,9 @@ int main(){
         cin >> coordinates[i][0];
         cin >> coordinates[i][1];
     }
-    printMatrix(coordinates);
-    
-    
+    //printMatrix(coordinates);
+    vector<vector<int>> adjMatrix = getAdjMatrix(coordinates, noVertices);
+    printMatrix(adjMatrix);
 
 
     return 0; 
