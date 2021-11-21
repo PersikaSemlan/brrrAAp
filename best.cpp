a759ebc863edf01c8a4ed91167e8eccddc1a4504
Emmaa <emma@emma.emma>
2021-11-21 18:13:04 +0100

    update to prims

--- best.cpp
+++ best.cpp
@@ -19,15 +19,39 @@ void printMatrix(vector<vector<int>> matrix){
         }
 }
 
-vector<vector<int>> primMST(){
+//Uses Prim's algorithm to calculate the minimum spanning tree (MST)
+vector<int> primMST( vector<vector<double>> coordinates, vector<vector<int>> adjMatrix, int noVertices){ 
+    vector<int> parents(noVertices); //stores parents to indices (children)
+    vector<bool> isIncluded(noVertices, false); //bitmap
+    vector<int> shortestDist(noVertices);
 
+    // 1. Initialize source vertex, and initialize distance to all other vertices to "infinity"
+    shortestDist[0] = 0; 
+    parents[0] = -1; 
+    isIncluded[0] = true;
+
+    for(int i = 1; i < noVertices; i++)
+        shortestDist[i] = INT32_MAX; 
+
+    // 2. Find shortest edge from source in adjMatrix 
+    int minDistance = INT32_MAX, closestIndex; 
     
+    for (int v = 0; v < noVertices; v++)
+        if (isIncluded[v] == false && shortestDist[v] < minDistance){
+            minDistance = shortestDist[v]; 
+            closestIndex = v;
+        }
+            
+    // 3. Find next shortest edge to unvisited node
+    // 4. Continue until isIncluded is all true
+
+
+    return parents;
 }
 
 
-
-//Create initial adjacency matrix
-//If self set distance to "infinity"
+//Create initial adjacency matrix           If self set distance to "infinity"
+// Optimize: change from 2D matrix to list to avoid double counting
 vector<vector<int>> getAdjMatrix(vector<vector<double>> coordinates, int noVertices){
     vector<vector<int>> adjMatrix(noVertices, vector<int>(noVertices));
     for (int i = 0; i < noVertices; i++) {
@@ -43,6 +67,7 @@ vector<vector<int>> getAdjMatrix(vector<vector<double>> coordinates, int noVerti
     return adjMatrix;
  }
 
+
 int main(){
     int noVertices;
     cin >> noVertices; 
@@ -52,9 +77,12 @@ int main(){
         cin >> coordinates[i][0];
         cin >> coordinates[i][1];
     }
+
     //printMatrix(coordinates);
     vector<vector<int>> adjMatrix = getAdjMatrix(coordinates, noVertices);
     printMatrix(adjMatrix);
+
+
 
 
     return 0; 
